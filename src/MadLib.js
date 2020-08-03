bc25876481ac0cba19298377e523957400a0553f
heyimmatt <mrmcfarland@gmail.com>
2020-08-03 11:44:47 -0700

    Setup state and handleChange for form, and add MadLibForm component

--- src/MadLib.js
+++ src/MadLib.js
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import MadLibForm from './MadLibForm';
 
 const MadLib = () => {
+  const INITIAL_FORM_DATA = {
+    story: 'select story',
+    adj1: '',
+    noun1: '',
+    verb1: '',
+    adj2: '',
+    name: ''
+  }
+  const [formData, setFormData] = useState(INITIAL_FORM_DATA);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value
+    })
+  }
+
   return (
-    <div>Hello!</div>
+    <MadLibForm formData={formData} handleChange={handleChange} />
   )
 }
 
