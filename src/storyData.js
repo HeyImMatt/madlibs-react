a8f7bf876aca7e939140fdaff145beb0b6b94f75
heyimmatt <mrmcfarland@gmail.com>
2020-08-03 22:33:50 -0700

    Add story mock data

---
+++ src/storyData.js
@@ -0,0 +1,14 @@
+const storyData = [
+  {
+    title: 'One Sentence Wonder',
+    inputWords: {adj1: '', name: '', verb1: ''},
+    text: ['This is a ', ' story called ', ' and it ', '!']
+  },
+  {
+    title: 'yeaaa',
+    inputWords: {name: '', adj1: '', noun1: '', verb1: ''},
+    text: ['Hello ', ' ! This is a ', ' story about nothing! It is basically ', '. So stop ',' and pay attention!']
+  },
+]
+
+export default storyData;
